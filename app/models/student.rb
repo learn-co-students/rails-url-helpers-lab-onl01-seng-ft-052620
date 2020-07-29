d0e9f0e4f4a938e090479bf0a409d50377cc8895
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-07-29 11:57:31 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_student
+    if self.active == true 
+      self.active = false 
+    else
+      self.active = true 
+
+    end
+    self.save
+  end
 end
