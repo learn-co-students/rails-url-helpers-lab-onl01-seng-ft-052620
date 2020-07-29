3cb1e5690b07210e629f5732292d5fda4e7dd331
jessegan <jesse13579@gmail.com>
2020-07-29 11:21:36 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,15 @@
 class Student < ActiveRecord::Base
+  attribute :active, :boolean, default: false
+
+  def active_switch
+    self.active = !self.active
+    self.save
+  end
+
+  def active_to_s
+    self.active ? "active" : "inactive"
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
